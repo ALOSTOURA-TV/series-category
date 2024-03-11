7299bf5390b3271c8531f2e780b80c31f017f400
Ostora <158589821+ALOSTOURA-TV@users.noreply.github.com>
2024-03-11 08:24:16 +0200

    Update Layout.js

--- Layout.js
+++ Layout.js
@@ -1,21 +1,6 @@
-<script type='text/javascript'>
- /*<![CDATA[*/
-function myFunction5() {
-  document.getElementById("Labels").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-    /*]]>*/
-</script>
+$(document).ready(function() {
+			$('#nav').onePageNav();
+		});
+$(document).ready(function() {
+			$('.slide-in').onePageNav();
+		});
